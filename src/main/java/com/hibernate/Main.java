1737571ff52a035021c7de3263ed1ce872b5548f
muskan0987654 <muskannandwal83801@gmail.com>
2025-02-14 11:15:20 +0530

    update

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -3,7 +3,13 @@ package com.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+
+
 
 import com.hibernate.model.Employee;
 
@@ -12,8 +18,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Employee Karishma=new Employee("Karishma","Female",55000);   //persistence object
-		Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sf=cfg.buildSessionFactory();
+//		Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sf=cfg.buildSessionFactory();
+//		SessionFactory sf=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
+		
+		StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sf=meta.buildSessionFactory();
+		
+		
 		Session session=sf.openSession();
 		Transaction tx=session.beginTransaction();
 		session.save(Karishma);
