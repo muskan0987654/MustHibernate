53b821f43e1b85f7113b0c63e97f8f96ffe8769c
muskan0987654 <muskannandwal83801@gmail.com>
2025-02-22 22:00:48 +0530

    update

--- src/main/java/com/hibernate/cfg/HibernateConfiguration.java
+++ src/main/java/com/hibernate/cfg/HibernateConfiguration.java
@@ -2,35 +2,35 @@ package com.hibernate.cfg;
 
 import java.util.Properties;
 
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.cfg.Environment;
 
 import org.hibernate.*;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateConfiguration {
 	
-	public static SessionFactory getSessionFactory() {
-		
+	
+	public static SessionFactory getSessionFactory(){
 		
 		Properties ps=new Properties();
 		
-		ps.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+		ps.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
 		ps.put(Environment.URL, "jdbc:mysql://localhost:3306/mdb");
 		ps.put(Environment.USER, "root");
 		ps.put(Environment.PASS, "root@1234");
 		ps.put(Environment.HBM2DDL_AUTO, "update");
-		ps.put(Environment.SHOW_SQL,"true");
+		ps.put(Environment.SHOW_SQL, "true");
 		ps.put(Environment.FORMAT_SQL, "true");
-		
+
 		
 		StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().applySettings(ps).build();
-		MetadataSources meta=new MetadataSources(ssr).addAnnotatedClass(com.hibernate.model.Employee.class).addAnnotatedClass(com.hibernate.model.Address.class);
+		MetadataSources meta= new MetadataSources(ssr).addAnnotatedClass(com.hibernate.model.Employee.class).addAnnotatedClass(com.hibernate.model.Address.class);
 		
 		
-		
-		return meta.getMetadataBuilder().build().buildSessionFactory() ;
+		return meta.getMetadataBuilder().build().buildSessionFactory();	
 		
 	}
 
